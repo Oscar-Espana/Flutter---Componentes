adba1c27029c91c315164ee355d3ddc182ef3ecc
Oscar España <espaaoscar@gmail.com>
2021-07-05 08:52:31 -0500

    rutas generadas automáticamente y archivo de rutas independiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
-import 'package:componentes/src/pages/home_page.dart';
+
 // import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
       title: 'Componentes',
       // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        print('Ruta llamada: ${settings.name}');
+        return MaterialPageRoute(builder: (context) => AlertPage());
       },
     );
   }
