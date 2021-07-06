c1c1dfaa260c181d31480d93f0caf2bfc62a6868
Oscar España <espaaoscar@gmail.com>
2021-07-05 22:43:47 -0500

    Image y FadeInImage Widgets

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,7 +9,13 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-        children: [_cardTipo1()],
+        children: [
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2()
+        ],
       ),
     );
   }
@@ -30,6 +36,28 @@ class CardPage extends StatelessWidget {
               TextButton(onPressed: () {}, child: Text('Ok')),
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: [
+          FadeInImage(
+              image: NetworkImage(
+                  'https://www.cose.ec/wp-content/uploads/2021/01/travel-landscape-01.jpg'),
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              fadeInDuration: Duration(milliseconds: 200),
+              height: 300,
+              fit: BoxFit.cover),
+          // Image(
+          //   image: NetworkImage(
+          //       'https://www.cose.ec/wp-content/uploads/2021/01/travel-landscape-01.jpg'),
+          // ),
+          Container(
+              padding: EdgeInsets.all(10.0), child: Text('Descripcion imagen'))
         ],
       ),
     );
