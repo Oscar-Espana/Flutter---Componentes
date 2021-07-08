7f25fcfacd3e307a95eb668d1bfdef6ab01a5f39
Oscar España <espaaoscar@gmail.com>
2021-07-07 21:56:58 -0500

    Widget: Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,10 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+          child: ElevatedButton(
+              child: Text('Mostrar Alerta'),
+              onPressed: () => _mostrarAlert(context))),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.remove_circle),
         onPressed: () {
@@ -14,5 +18,35 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Titulo'),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Contenido de la alerta'),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')),
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Ok')),
+            ],
+          );
+        },
+        barrierDismissible: true);
   }
 }
