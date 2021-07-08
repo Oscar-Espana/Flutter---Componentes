44064e075bcded68018dc1056a5849edf17b9efc
Oscar España <espaaoscar@gmail.com>
2021-07-07 22:10:42 -0500

    Widget Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -4,9 +4,33 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Avatar Page'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Avatar Page'),
+          actions: [
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://dam.esquirelat.com/wp-content/uploads/2020/07/STANLEE.jpg'),
+                radius: 25.0,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 10.0),
+              child: CircleAvatar(
+                child: Text('SL'),
+                backgroundColor: Colors.amberAccent,
+              ),
+            )
+          ],
+        ),
+        body: Center(
+          child: FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://www.elsoldeacapulco.com.mx/incoming/wpgvi0-villanos-creados-por-stan-lee.jpg/ALTERNATES/LANDSCAPE_1140/Villanos%20creados%20por%20Stan%20lee.jpg'),
+            fadeInDuration: Duration(milliseconds: 200),
+          ),
+        ));
   }
 }
