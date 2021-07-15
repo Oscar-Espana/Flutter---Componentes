a7ce34abe61e9a1ef1e795bb41dfd46cac755050
Oscar España <espaaoscar@gmail.com>
2021-07-14 21:45:09 -0500

    Slider

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider'),
+      ),
+      body: Container(
+          padding: EdgeInsets.only(top: 20.0),
+          child: Column(
+            children: [_crearSlider(), Expanded(child: _crearImagen())],
+          )),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+        activeColor: Colors.indigo,
+        label: 'Tamaño de la imagen',
+        value: _valorSlider,
+        min: 10.0,
+        max: 400.0,
+        onChanged: (valor) {
+          setState(() {
+            _valorSlider = valor;
+            print(_valorSlider);
+          });
+        });
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage(
+          'https://occ-0-1068-1722.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABRkEbFIM_K8hENt1nCabcWsHFuOUQ2XBEjTsHs_o_yqd_qtSMNTmeQyzi4sX5ZxalDF9HrUdS05PD60RO61Bx7u3lsqH.jpg?r=ea4'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+}
